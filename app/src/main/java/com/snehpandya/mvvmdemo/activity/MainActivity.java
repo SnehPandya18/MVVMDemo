9ae7e747f02778d866c79643d7de71fae2ad5844
Sneh Pandya <sneh.pandya1@gmail.com>
2017-09-12 19:18:05 +0530

    Working: Implement partial MVVM logic

--- app/src/main/java/com/snehpandya/mvvmdemo/activity/MainActivity.java
+++ app/src/main/java/com/snehpandya/mvvmdemo/activity/MainActivity.java
@@ -1,24 +1,37 @@
 package com.snehpandya.mvvmdemo.activity;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.snehpandya.mvvmdemo.R;
+import com.snehpandya.mvvmdemo.data.ArticleAdapter;
 import com.snehpandya.mvvmdemo.databinding.MainAB;
-import com.snehpandya.mvvmdemo.viewmodel.MainViewModel;
+import com.snehpandya.mvvmdemo.model.Article;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MainAB binding;
-    private MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mViewModel = new MainViewModel();
-        mViewModel.setGreet("Hola!");
-        binding.setViewModel(mViewModel);
+        MainAB binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerview.setLayoutManager(layoutManager);
+
+
+        List<Article> articleList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            articleList.add(new Article("Article " + i, i, true));
+        }
+
+        ArticleAdapter articleAdapter = new ArticleAdapter(articleList, this);
+        binding.recyclerview.setAdapter(articleAdapter);
     }
 }
